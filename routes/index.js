2a6b54433115d58586d93ad6fe6ea602ab485d5f
JamieYau <jamie.yau2002@me.com>
2024-01-16 02:44:08 +0000

    Add new message form

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const messages = [
@@ -15,8 +15,18 @@ const messages = [
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', messages });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Message Board", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message" });
+});
+
+router.post("/new", function (req, res, next) {
+  const { author, message } = req.body;
+  messages.push({ text: message, user: author, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
